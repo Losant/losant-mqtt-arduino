a0e12fb87695f7f1a740ef8332d7c7ec1156d866
Heath Blandford <35548666+whoisheath@users.noreply.github.com>
2021-10-29 11:18:52 -0500

    update esp32 example code to use RootCA needed for connection

--- examples/esp32/esp32.ino
+++ examples/esp32/esp32.ino
@@ -15,18 +15,42 @@
 #include <Losant.h>
 
 // WiFi credentials.
-const char* WIFI_SSID = "my-wifi-ssid";
-const char* WIFI_PASS = "my-wifi-pass";
+const char* WIFI_SSID = "";
+const char* WIFI_PASS = "";
 
 // Losant credentials.
-const char* LOSANT_DEVICE_ID = "my-device-id";
-const char* LOSANT_ACCESS_KEY = "my-app-key";
-const char* LOSANT_ACCESS_SECRET = "my-app-secret";
+const char* LOSANT_DEVICE_ID = "";
+const char* LOSANT_ACCESS_KEY = "";
+const char* LOSANT_ACCESS_SECRET = "";
 
 const int BUTTON_PIN = 14;
 const int LED_PIN = 12;
 
 bool ledState = false;
+
+const char* rootCABuff = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+"QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
+"MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
+"b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
+"9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
+"CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
+"nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
+"43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
+"T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
+"gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
+"BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
+"TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
+"DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
+"hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
+"06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+"PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+"YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+"CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+"-----END CERTIFICATE-----\n";
 
 // initiate the the wifi client
 WiFiClientSecure wifiClient;
@@ -69,7 +93,10 @@ void connect() {
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
 
+//  wifiClient.setInsecure();
+
   WiFi.begin(WIFI_SSID, WIFI_PASS);
+  wifiClient.setCACert(rootCABuff);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
